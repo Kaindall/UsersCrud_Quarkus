61d808693653396dfbbd602356f43442578d3554
Kaindall <gustavozanete2@gmail.com>
2025-02-02 16:47:45 -0300

    FIX: arrumar os testes para passarem

--- src/test/java/br/com/kaindall/GreetingResourceTest.java
+++ src/test/java/br/com/kaindall/GreetingResourceTest.java
@@ -4,17 +4,17 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+//import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 class GreetingResourceTest {
     @Test
-    void testHelloEndpoint() {
+    void findAllUsersShouldReturn200() {
         given()
-          .when().get("/hello")
+          .when().get("/usuarios")
           .then()
-             .statusCode(200)
-             .body(is("Hello from Quarkus REST"));
+             .statusCode(200);
+             //.body(is("Hello from Quarkus REST"));
     }
 
 }
